232b5cec7b74cb307d381530a0e7f6e7e0ad0ac7
EdgarasKit <edgar.kitkevich@gmail.com>
2022-03-22 11:05:49 +0200

    1 funkcija users amziaus vidurkis

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,9 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+let getUserAverageAge = (masyvas) => {
+  let amziausVidurkis = 0;
+  masyvas.forEach(item => amziausVidurkis += item.age)
+  return (amziausVidurkis / masyvas.legth).toFixed(0)
+}
