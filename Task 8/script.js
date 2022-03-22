27d2fbe2a963032e05ee4f7606bee5c9a1c6023a
EdgarasKit <edgar.kitkevich@gmail.com>
2022-03-22 13:41:32 +0200

    padaryta klase kalkuliatorius

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,23 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum = (a, b) => {
+        let atsakymas1 = a + b;
+        return atsakymas1;
+    };
+
+    subtraction = (a, b) => {
+        let atsakymas2 = a - b;
+        return atsakymas2;
+    };
+    multiplication = (a, b) => {
+        let atsakymas3 = a * b;
+        return atsakymas3;
+    };
+    division = (a, b) => {
+        let atsakymas4 = a / b;
+        return atsakymas4;
+    };
+}
