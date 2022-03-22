8ab641cfe89a35833083d86fe9be16dc234be52f
EdgarasKit <edgar.kitkevich@gmail.com>
2022-03-22 09:12:58 +0200

    js funkcija veikia

--- Task 2/script.js
+++ Task 2/script.js
@@ -7,3 +7,8 @@ pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 let skaiciuoti = 0;
         const btn = document.getElementById("btn__element");
         const disp = document.getElementById("btn__state");
+
+        btn.onclick = function () {
+            skaiciuoti++;
+            disp.innerHTML = skaiciuoti;
+        };
